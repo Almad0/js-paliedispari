e1ca9479177ddde4e2956f069af8788ecbb06fd9
Alessandro <internicola.alessandro@gmail.com>
2020-11-20 17:09:30 +0100

    P/D completo, ESERCIZIO COMPLETO
    Usato le funzioni come da consegna, definito un booleanto per la scelta del pari/dispari. Definito vincitore tramite if

--- Pari&Dispari/assets/js/main.js
+++ Pari&Dispari/assets/js/main.js
@@ -1,6 +1,43 @@
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+// FUNZIONE DI GENERAZONE NUMERO RANDOM
+function randomNum (max, min){
+  return parseInt(Math.random() * (max - min) + min);
+}
+
+// FUNZIONE PARI E DISPARI
+function PariDispari(num1, num2) {
+  var somma = num1 + num2;
+  if (somma % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// FUNZIONE PER TRASFORMARE L'INPUT IN VALORE BOOLEANO
+function booleanInput (word){
+  if (word == "P") {
+    return true;
+  } else if (word == "D"){
+    return false;
+  }
+}
+
+
+var userEvenOdd = prompt("Scegli, pari o dispari (P/D)").toUpperCase();
+var userNum = Number(prompt("Inserisci un numero da uno a cinque"));
+
+// RICHIAMO LE FUNIONI
+var pcNum = randomNum(1, 6);
+var userChoice = booleanInput(userEvenOdd)
+var risultato = PariDispari (userNum, pcNum);
+// console.log(userNum);
+// console.log(pcNum);
+// console.log(userChoice);
+// console.log(risultato);
+
+// SCELGO CHI A VINTO TRAMITE VALORI BOOLEANI
+if (userChoice == risultato) {
+  console.log("Hai vinto, bravo!");
+} else {
+  console.log("Hai perso, fai schifo");
+};
